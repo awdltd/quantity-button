69e99edffe1e2cf245d767745d1c505c8410eb1e
Oliver <oliver.hepworth@googlemail.com>
2019-10-02 23:12:24 +0100

    Converted to custom element; improved accessibility with aria-labels

--- quantity.js
+++ quantity.js
@@ -3,42 +3,50 @@
  *  @function Quantity
  *  @param {DOMobject} element to create a quantity wrapper around
  */
-export default class QuantityInput {
-  constructor(self, decreaseText, increaseText) {
+export default class QuantityInput extends HTMLElement {
+  constructor(decreaseText, increaseText) {
+    super();
+
     // Create input
     this.input = document.createElement('input');
+    this.input.setAttribute('type', 'number');
+    this.input.setAttribute('name', 'quantity');
+    this.input.setAttribute('pattern', '[0-9]+');
+    this.input.setAttribute('aria-label', 'Current quantity');
+
+    // Set default attribute
     this.input.value = 1;
-    this.input.type = 'number';
-    this.input.name = 'quantity';
-    this.input.pattern = '[0-9]+';
 
     // Get text for buttons
     this.decreaseText = decreaseText || 'Decrease quantity';
     this.increaseText = increaseText || 'Increase quantity';
 
     // Button constructor
-    function Button(text, className){
+    function Button(text, label = 'change quantity', className){
       this.button = document.createElement('button');
-      this.button.type = 'button';
-      this.button.innerHTML = text;
-      this.button.title = text;
+      this.button.textContent = text;
       this.button.classList.add(className);
+
+      this.button.setAttribute('title', text);
+      this.button.setAttribute('aria-label', label);
+      this.button.setAttribute('role', 'button');
+      this.button.setAttribute('type', 'button');
 
       return this.button;
     }
 
     // Create buttons
-    this.subtract = new Button(this.decreaseText, 'sub');
-    this.add = new Button(this.increaseText, 'add');
+    this.subtract = new Button(this.decreaseText, 'Decrease quantity', 'sub');
+    this.add = new Button(this.increaseText, 'Increase quantity', 'add');
 
     // Add functionality to buttons
     this.subtract.addEventListener('click', () => this.change_quantity(-1));
     this.add.addEventListener('click', () => this.change_quantity(1));
 
     // Add input and buttons to wrapper
-    self.appendChild(this.subtract);
-    self.appendChild(this.input);
-    self.appendChild(this.add);
+    this.appendChild(this.subtract);
+    this.appendChild(this.input);
+    this.appendChild(this.add);
   }
 
   change_quantity(change) {
@@ -56,5 +64,21 @@ export default class QuantityInput {
 
     // Output number
     this.input.value = quantity;
+  }
+
+  set value(val) {
+    this.input.value = val;
+  }
+
+  get value() {
+    return this.input.value;
+  }
+
+  get name() {
+    return this.input.name;
+  }
+
+  get id() {
+    return this.input.id;
   }
 }
